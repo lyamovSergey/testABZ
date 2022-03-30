6cb396fb99e724bdf9911f7ebef0eecb89af73a0
lyamov.s <lyamov.s@sistema-vpool>
2022-03-30 12:39:37 +0300

    minor

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,7 @@
 let apiUrl = "https://frontend-test-assignment-api.abz.agency/api/v1";
-let apiKey = "123456";
 let usersCount = 6;
 
 export default {
     apiUrl: apiUrl,
-    apiKey: apiKey,
     usersCount: usersCount
 };
